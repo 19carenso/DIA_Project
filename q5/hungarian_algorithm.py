4c20727eec365f23f8e58f82a42d499f843ca8f4
Jérémy Lengelé <jeremylengele@gmail.com>
2021-09-14 17:35:56 +0200

    Correction dans l'algo hongrois
    Step3 + erreurs d'indentation corrigée + test algo hongrois

--- q5/hungarian_algorithm.py
+++ q5/hungarian_algorithm.py
@@ -10,37 +10,60 @@ def step2(m):
         m[:,j] = m[:,j] - np.min(m[:,j])
 
 
+
 def step3(m):
     dim = m.shape[0]
-    assigned = np.array([])
-    assignments = np.zeros(m.shape, dtype = int)
+    zeros = {}
 
-    for i in range(0,dim):
-        for j in range(0, dim):
-            if( m[i,j] == 0 and np.sum(assignments[:,j]) == 0 and np.sum(assignments[i,:]) == 0) :
-                assignments[i,j] = 1
-                assigned = np.append(assigned, i)
+    for i in range(dim):
+        for j in range(dim):
+            if m[i,j] == 0 :
+                zeros[i*dim+j] = 1
 
-    
-    rows = np.linspace(0, dim-1, dim).astype(int)
-    marked_rows = np.setdiff1d(rows, assigned)
-    new_marked_rows = marked_rows.copy()
-    marked_cols = np.array([])
+    rows = np.array([0,1,2,3], dtype = int)
+    columns = np.array([], dtype= int)
 
-    while(len(new_marked_rows)>0):
-        new_marked_cols = np.array([], dtype = int)
-        for nr in new_marked_rows:
-            zeros_cols = np.argwhere(m[nr,:]==0).reshape(-1)
-            new_marked_cols = np.append(new_marked_cols, np.setdiff1d(zeros_cols, marked_cols))
-        marked_cols = np.append(marked_cols, new_marked_cols)
-        new_marked_rows = np.array([], dtype = int)
-        
-        for nc in new_marked_cols:
-            new_marked_rows = np.append(new_marked_rows, np.argwhere(assignments[:,nc]==1).reshape(-1))
-        
-        marked_rows = np.unique(np.append(marked_rows, new_marked_rows))
-        
-    return np.setdiff1d(rows, marked_rows).astype(int), np.unique(marked_cols)
+    #3 lines selected
+    for i in range(dim):
+        for j in range(dim):
+
+            for k in range(i+1,dim):
+                b = True
+                for zero in zeros.keys():
+                    if zero//dim != i and zero%dim != j and zero//dim != k:
+                        b = False
+                        break
+                if b:
+                    rows = np.array([i,k])
+                    columns = np.array([j])
+                    break
+
+            for k in range(j+1,dim):
+                b = True
+                for zero in zeros.keys():
+                    if zero//dim != i and zero%dim != j and zero%dim != k:
+                        b = False
+                        break
+                if b:
+                    rows = np.array([i])
+                    columns = np.array([j,k])
+                    break
+
+    #2 lines selected
+    for i in range(dim):
+        for j in range(dim):
+            b = True
+            for zero in zeros.keys():
+                if zero//dim != i and zero%dim != j:
+                    b = False
+                    break
+            if b:
+                rows = np.array([i])
+                columns = np.array([j])
+                break
+
+    return rows, columns
+
 
 def step5(m, covered_rows, covered_cols): 
     dim0 = m.shape[0]
@@ -56,14 +79,14 @@ def step5(m, covered_rows, covered_cols):
     
     for i in uncovered_rows.astype(int):
         for j in uncovered_cols.astype(int):
-            if(m[i,j]<min_val):
+            if m[i,j] < min_val:
                 min_val = m[i,j]
                 
-        for i in uncovered_rows.astype(int):
-            m[i,:] -= min_val
-        
-        for j in covered_cols.astype(int):
-            m[:,j] += min_val
+    for i in uncovered_rows.astype(int):
+        m[i,:] -= min_val
+    
+    for j in covered_cols.astype(int):
+        m[:,j] += min_val
             
 
 def find_rows_single_zero(matrix):
@@ -71,17 +94,15 @@ def find_rows_single_zero(matrix):
         if np.sum(matrix[i,:] == 0) == 1:
             j = np.argwhere(matrix[i,:]==0).reshape(-1)
             return(i,j)
-        return False
+    return False
     
 def find_cols_single_zero(matrix):
     for j in range(0, matrix.shape[1]):
         if np.sum(matrix[:,j] == 0) == 1:
             i = np.argwhere(matrix[:,j]==0).reshape(-1)
             return(i,j)
-        return False    
+    return False    
     
-
-
 def assignment_single_zero_lines(m, assignment) :
     val = find_rows_single_zero(m)
     while(val):
@@ -91,8 +112,7 @@ def assignment_single_zero_lines(m, assignment) :
         assignment[i,j] = 1
         val = find_rows_single_zero(m)
         
-    val = find_cols_single_zero(m)
-    
+    val = find_cols_single_zero(m)  
     while(val):
         i,j = val[0], val[1]
         m[i,:] += 1
@@ -103,22 +123,19 @@ def assignment_single_zero_lines(m, assignment) :
 
 
 def first_zero(m):
-    return np.argwhere(m==0) [0][0], np.argwhere(m==0) [0][1]
-    
-    
-    
-    
+    return np.argwhere(m==0) [0][0], np.argwhere(m==0) [0][1]  
+
+
 def final_assignment(initial_matrix, m):
     assignment = np.zeros(m.shape, dtype = int)
     assignment = assignment_single_zero_lines(m, assignment)
-    while(np.sum(m==0)>0):
+    while(np.sum(m == 0) > 0):
         i,j = first_zero(m)
         assignment[i,j] = 1
         m[i, :] += 1
         m[:,j] += 1
-        assignment = assignment_single_zero_lines(m, assignment)
-        
-    return assignment* initial_matrix, assignment
+        assignment = assignment_single_zero_lines(m, assignment)       
+    return assignment
 
 def hungarian_algorithm(matrix):
     m = matrix.copy()
@@ -126,38 +143,22 @@ def hungarian_algorithm(matrix):
     step2(m)
     n_lines = 0 
     max_length = np.maximum(m.shape[0], m.shape[1])
+    alpha = 0
     while n_lines != max_length:
+        alpha = alpha + 1
         lines = step3(m)
+        
         n_lines = len(lines[0]) + len(lines[1])
         if n_lines != max_length: 
             step5(m, lines[0], lines[1])
-        return final_assignment(matrix, m)
+
+    return final_assignment(matrix, m)
     
-a = np.random.randint(4, size = (4,4))
+
+test = np.array([[ 0, 10, 25, 46],
+                 [ 0, 24, 55, 91],
+                 [ 0, 23, 49, 77],
+                 [ 0, 26, 54, 84]])
 
 
-res = hungarian_algorithm(a)
-#print("\n Optimal Matching \n", res[1], "\n Value:", np.sum(res[0]))
-
-
-
-test = np.array([[ 6, 14, 22, 26],
-                 [ 8, 18, 28, 34],
-                 [ 4, 14, 24,  0],
-                 [0,0,0,0]
-                 ])
-
-id = hungarian_algorithm(test)
-compteur=0
-while np.sum(id[1],axis=0).all() != np.array([1, 1, 1, 1]).all(): 
-    test = test +np.random.normal(0,.1)
-    id = hungarian_algorithm(test)
-    compteur+=1
-    print('Le compteur vaut',compteur)
- 
-
-bub = np.array([[ 6, 14, 22, 26],
-       [ 8, 18, 28, 34],
-       [ 4, 14, 24,  0],
-       [ 6, 17, 23,  5]])
 
--- q5/q5.py
+++ q5/q5.py
@@ -17,33 +17,33 @@ p1 = 45
 p2_initial = 40
 
   
-    
-
 def assignment(p1, p2_initial, n_clients_per_class) :
-    res = (np.zeros(16).reshape(4,4),np.zeros(16).reshape(4,4)) #initialization to enter into the while loop
-    #Sometimes, some class isn't associated to any promo so we might need to re-simulate the number of customers per class
-    while np.array_equal(res[1].sum(axis=1),  np.array([1, 1, 1, 1]), equal_nan=False)==False:
-        adj_mat = np.zeros(16)   
-        n_clients_per_class_rnd = list(np.round(n_clients_per_class + np.random.normal(0,20,4))) #simulation of the number of customer per class according to a normal distribution
-        for i in range (0,4):
-            #i,1
-            alpha  = [1, 0, 0, 0]
-            adj_mat[4*i+0] = profit_per_class(i,p1, p2_initial, alpha, n_clients_per_class_rnd)
-            #i,2
-            alpha  = [0, 1, 0, 0]
-            adj_mat[4*i+1] = profit_per_class(i,p1, p2_initial, alpha, n_clients_per_class_rnd)
-            #i,3
-            alpha  = [0, 0, 1, 0]
-            adj_mat[4*i+2] = profit_per_class(i,p1, p2_initial, alpha, n_clients_per_class_rnd)            
-            #i,4
-            alpha  = [0, 0, 0, 1]                     
-            adj_mat[4*i+3] = profit_per_class(i,p1, p2_initial, alpha, n_clients_per_class_rnd)
+    res = (np.zeros(16).reshape(4,4),np.zeros(16).reshape(4,4))
+    
+    adj_mat = np.zeros(16)   
+    n_clients_per_class_rnd = list(np.round(n_clients_per_class + np.random.normal(0,20,4))) #simulation of the number of customer per class according to a normal distribution
+    for i in range (0,4):
+        #i,1
+        alpha  = [1, 0, 0, 0]
+        adj_mat[4*i+0] = profit_per_class(i,p1, p2_initial, alpha, n_clients_per_class_rnd)
+        #i,2
+        alpha  = [0, 1, 0, 0]
+        adj_mat[4*i+1] = profit_per_class(i,p1, p2_initial, alpha, n_clients_per_class_rnd)
+        #i,3
+        alpha  = [0, 0, 1, 0]
+        adj_mat[4*i+2] = profit_per_class(i,p1, p2_initial, alpha, n_clients_per_class_rnd)            
+        #i,4
+        alpha  = [0, 0, 0, 1]                     
+        adj_mat[4*i+3] = profit_per_class(i,p1, p2_initial, alpha, n_clients_per_class_rnd)
 
-        adj_mat = adj_mat.reshape((4,4))
-        #adj_mat = np.max(adj_mat) - adj_mat #Autre possibilité de fonction de perte qui donne les mêmes résultats
-        adj_mat  = - adj_mat
-        res = hungarian_algorithm(adj_mat)
-    return res
+        
+    adj_mat = adj_mat.reshape((4,4))
+    adj_mat = np.max(adj_mat) - adj_mat #Autre possibilité de fonction de perte qui donne les mêmes résultats
+    res = hungarian_algorithm(adj_mat)
+    
+    return res, adj_mat
 
 
 res = assignment(p1, p2_initial, n_clients_per_class)
+print(res[0],"\n",res[1])
+
